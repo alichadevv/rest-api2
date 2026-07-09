fc53519f71d2528dfb81ae09547b5b93ad74e706
メイチャ <alichaicha248@gmail.com>
2026-07-10 04:17:12 +0700

    Add files via upload

--- routers/games/tebakkimia.js
+++ routers/games/tebakkimia.js
@@ -33,7 +33,7 @@ export default {
         JSON.stringify(
           {
             status: true,
-            creator: "Raihan Fadillah",
+            creator: "Mas Yuu",
             result,
             total: data.length,
           },
